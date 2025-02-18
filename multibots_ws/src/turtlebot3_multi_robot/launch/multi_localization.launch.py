7a8b681e909fe8cb733fb06c3eb39b352d733d4b
Hyuna <sjajmh6612@naver.com>
2025-02-18 21:23:42 +0900

    source code

---
+++ multibots_ws/src/turtlebot3_multi_robot/launch/multi_localization.launch.py
@@ -0,0 +1,75 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    tb3_0_config = os.path.join(get_package_share_directory('turtlebot3_multi_robot'), 'config', 'tb3_0_amcl.yaml')
+    tb3_1_config = os.path.join(get_package_share_directory('turtlebot3_multi_robot'), 'config', 'tb3_1_amcl.yaml')
+    tb3_2_config = os.path.join(get_package_share_directory('turtlebot3_multi_robot'), 'config', 'tb3_2_amcl.yaml')
+
+    map_file = os.path.join(get_package_share_directory('turtlebot3_multi_robot'), 'map', 'map.yaml')
+
+    return LaunchDescription([
+        
+        Node(
+            package='nav2_map_server',
+            executable='map_server',
+            name='map_server',
+            output='screen',
+            parameters=[{'use_sim_time': True},
+                        {'topic_name': "map"},
+                        {'frame_id': "map"},
+                        {'yaml_filename': map_file}]
+        ),
+
+        
+        Node(
+            namespace='tb3_0',
+            package='nav2_amcl',
+            executable='amcl',
+            name='amcl',
+            output='screen',
+            parameters=[tb3_0_config]
+        ),
+        Node(
+            namespace='tb3_1',
+            package='nav2_amcl',
+            executable='amcl',
+            name='amcl',
+            output='screen',
+            parameters=[tb3_1_config]
+        ),
+        Node(
+            namespace='tb3_2',
+            package='nav2_amcl',
+            executable='amcl',
+            name='amcl',
+            output='screen',
+            parameters=[tb3_2_config]
+        ),
+
+      
+        Node(
+            package='nav2_lifecycle_manager',
+            executable='lifecycle_manager',
+            name='lifecycle_manager_localization',
+            output='screen',
+            parameters=[{'use_sim_time': True},
+                        {'autostart': True},
+                        {'bond_timeout': 0.0},
+                        {'node_names': ['map_server', 'tb3_0/amcl', 'tb3_1/amcl', 'tb3_2/amcl']}]
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', os.path.join(get_package_share_directory('turtlebot3_multi_robot'), 'rviz', 'multibots.rviz')]
+        )
+
+     
+
+        
+    ])
---
+++ multibots_ws/src/turtlebot3_multi_robot/launch/multi_spawn.launch.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, RegisterEventHandler
+from launch.substitutions import LaunchConfiguration, Command
+from launch.actions import IncludeLaunchDescription, ExecuteProcess
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch.event_handlers import OnProcessExit
+from launch.conditions import IfCondition
+import launch.logging
+import xacro
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    
+    robots = [
+        {'name': 'tb3_0', 'x_pose': '9.5', 'y_pose': '-6.0', 'z_pose': '0.01', 'yaw_pose': '2.5', 'model': 'turtlebot3_waffle_0'},
+        {'name': 'tb3_1', 'x_pose': '9.0', 'y_pose': '-7.0', 'z_pose': '0.01', 'yaw_pose': '2.5', 'model': 'turtlebot3_waffle_1'},
+        {'name': 'tb3_2', 'x_pose': '-2.0', 'y_pose': '0.0', 'z_pose': '0.01', 'yaw_pose': '0.01', 'model': 'turtlebot3_waffle_2'}
+    ]
+
+    TURTLEBOT3_MODEL = 'waffle'
+    turtlebot3_gazebo_dir = get_package_share_directory('turtlebot3_multi_robot')
+    
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    declare_use_sim_time = DeclareLaunchArgument(
+        name='use_sim_time', default_value=use_sim_time, description='Use simulator time'
+    )
+
+    enable_drive = LaunchConfiguration('enable_drive', default='false')
+    declare_enable_drive = DeclareLaunchArgument(
+        name='enable_drive', default_value=enable_drive, description='Enable robot drive node'
+    )
+    
+
+    world = os.path.join(
+        get_package_share_directory('turtlebot3_multi_robot'),
+        'worlds', 'moon.world')
+
+   
+    gzserver_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gzserver.launch.py')
+        ),
+        launch_arguments={'world': world}.items(),
+    )
+
+    
+    gzclient_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gzclient.launch.py')
+        ),
+    )
+
+    ld.add_action(declare_use_sim_time)
+    ld.add_action(declare_enable_drive)
+    ld.add_action(gzserver_cmd)
+    ld.add_action(gzclient_cmd)
+ 
+    
+    last_action = None  
+    
+    for robot in robots:
+        namespace = '/' + robot['name']
+        
+        turtlebot3_urdf = os.path.join(turtlebot3_gazebo_dir, 'urdf', f"{robot['model']}.urdf")
+
+       
+       
+        urdf_content = Command(['xacro ', turtlebot3_urdf, ' robot_name:=', robot['name']])
+
+
+        
+        turtlebot3_joint_state_publisher = Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher',
+            namespace=namespace,
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time}]
+        )               
+        
+        turtlebot3_state_publisher = Node(
+            package='robot_state_publisher',
+            namespace=namespace,
+            executable='robot_state_publisher',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time,
+                             'publish_frequency': 10.0,
+                             'robot_description': urdf_content
+                             }],
+                              
+            #remappings=[('/tf','tf'), ('/tf_static','tf_static')]
+        )
+
+        
+        spawn_turtlebot3 = Node(
+            package='gazebo_ros',
+            executable='spawn_entity.py',
+            arguments=[
+                "-topic", namespace + "/robot_description",
+                '-entity', robot['name'],
+                '-robot_namespace', robot['name'],
+                '-x', robot['x_pose'], '-y', robot['y_pose'],
+                '-z', '0.01', '-Y', robot['yaw_pose'],
+                '-unpause',
+            ],
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time}],
+            remappings=[('/tf','tf'),('/tf_static', 'tf_static')]
+        )
+
+        if last_action is None:
+            
+            ld.add_action(turtlebot3_joint_state_publisher)
+            ld.add_action(turtlebot3_state_publisher)
+            ld.add_action(spawn_turtlebot3)
+           
+        else:
+            
+            spawn_turtlebot3_event = RegisterEventHandler(
+                event_handler=OnProcessExit(
+                    target_action=last_action  if last_action is not None else spawn_turtlebot3,
+                    on_exit=[turtlebot3_joint_state_publisher,turtlebot3_state_publisher, spawn_turtlebot3],
+                )
+            )
+            ld.add_action(spawn_turtlebot3_event)
+
+        
+        last_action = spawn_turtlebot3
+
+    return ld
